464d66711d5ae24e5164677fc121c3e741fec341
John <john2baik@gmail.com>
2017-02-19 23:24:51 -0500

    updating db
    db update

--- db/schema.rb
+++ db/schema.rb
@@ -19,4 +19,37 @@ ActiveRecord::Schema.define(version: 20170215053153) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.integer "user_id",                          null:false
+    t.integer "age"
+    t.text "location"
+    t.text "name",                                null:false
+    t.text "phone_number"
+    t.integer "rating"
+    t.text "user_bio"
+    t.text "instrument"
+    t.text "level"
+  end
+
+  create_table "jam_sessions", force: :cascade do |t|
+    t.integer "jam_sessions_id",                  null: false
+    t.integer "init_user_id",                     null: false
+    t.integer "part_user_id",                     null: false
+    t.datetime "sesh_time",                       null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
+    #t.datetime "created_at",                      null: false
+    #t.datetime "updated_at",                      null: false
+  end
+
+  create_table "friends", force: :cascade do |t|
+    t.integer  "friendable_id",                   null: false
+    t.integer  "friender_id",                     null: false
+    t.boolean  "blocked",         default: false, null: false
+  end
+
+  add_index "friends", ["friendable_id","friender_id"], name: "friender friends friendable", using: :btree
+
 end
