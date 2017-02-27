5502ad64ca4573d343f2226da9821e88b637e50f
John <john2baik@gmail.com>
2017-02-26 19:38:08 -0500

    working database with signup screen
    can update users to database

--- Gemfile
+++ Gemfile
@@ -9,9 +9,9 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.1'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3', group: development
+gem 'sqlite3' #group: development
 # Use PostgreSQL for deployment
-gem 'pg', group: production
+#gem 'pg', group: production
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
@@ -37,8 +37,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
-# Use ActiveModel has_secure_password
-gem 'bcrypt', '~> 3.1.7'
+
 
 # Use Bootstrap to generate a responsive web page
 gem 'bootstrap-sass', '3.3.6'
--- db/schema.rb
+++ db/schema.rb
@@ -10,32 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170226053608) do
-
-  create_table "friends", force: :cascade do |t|
-    t.integer "friendable_id",                 null: false
-    t.integer "friender_id",                   null: false
-    t.boolean "blocked",       default: false, null: false
-    t.index ["friendable_id", "friender_id"], name: "friender friends friendable"
-  end
-
-  create_table "genres", force: :cascade do |t|
-    t.string "name"
-  end
-
-  create_table "jam_sessions", force: :cascade do |t|
-    t.integer  "jam_sessions_id", null: false
-    t.integer  "init_user_id",    null: false
-    t.integer  "part_user_id",    null: false
-    t.datetime "sesh_time",       null: false
-  end
-
-  create_table "static_pages", force: :cascade do |t|
-    t.string   "home"
-    t.string   "help"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20170227002129) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -43,7 +18,6 @@ ActiveRecord::Schema.define(version: 20170226053608) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
