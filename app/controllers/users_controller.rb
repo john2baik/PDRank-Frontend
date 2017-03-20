9c9128a61af8d91bc35bd242bde816d5769f2e94
John <john2baik@gmail.com>
2017-03-19 21:12:41 -0400

    changes to frontend
    now it is PDReader

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :age, :location, :sex, :profession, :bio)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
 
