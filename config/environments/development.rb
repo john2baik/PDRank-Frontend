6dab16253d9a0f46623243e4c6d9482316e7f736
peteryzheng <yzhen74@emory.edu>
2017-04-17 21:48:19 -0400

    commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
       s3_region: ENV["AWS_S3_REGION"],
       s3_credentials: {
           s3_host_name: ENV["AWS_S3_HOST_NAME"],
-          bucket: ENV["AWS_S3_BUCKET"],
+         bucket: ENV["AWS_S3_BUCKET"],
           access_key_id: ENV["AWS_ACCESS_KEY_ID"],
           secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
       }
