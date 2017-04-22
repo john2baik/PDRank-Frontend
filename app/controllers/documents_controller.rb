29b39681ebe7a544043becaad49ae1a8fa71c7d8
John <john2baik@gmail.com>
2017-04-21 21:45:43 -0400

    updates
    working on user profile

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,12 +1,16 @@
 class DocumentsController < ApplicationController
 
   def create
-    @document = Document.new(document_params)
-    @document.save
+    @user = User.find_by_id(params[:user_id])
+    @document = @user.documents.create(:params[:document_params])
+    if @document.save
+      flash[:success] = "Document Saved!"
+      redirect_to @current_user
+    end
   end
 
   def new
-    @docuemnt = Document.new(document_params)
+    @document = Document.new
   end
 
   def destroy
@@ -25,6 +29,6 @@ class DocumentsController < ApplicationController
   private
 
   def document_params
-    params.require(:document).permit(:file)
+    params.require(:document).permit(:file_file_name, :file_content_type, :file_file_size)
   end
 end
